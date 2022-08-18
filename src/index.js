89203f918fb65a4bf131898dae0573d251d46c83
aleksei-v <aleksey.vasilenko7@gmail.com>
2022-08-18 22:50:58 +0300

    add lightbox, some updates and checks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
 
+import "simplelightbox/dist/simple-lightbox.min.css";
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '29344845-320c81c87bec30b6c30cddfd7';
 const axios = require('axios');
+let page = 1;
 
 const refs = {
     searchButton: document.querySelector('.submit-button'),
@@ -14,15 +17,17 @@ async function fetchImages(name) {
     try {
     const url = `${BASE_URL}?key=${API_KEY}&q=${name}`;
     const images = await axios.get(url, {
-    params: {
+        params: {
+        per_page: 40,
+        page,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
     }})
-    return images.data.hits;
+    return images;
     }
     catch {
-        clearImageGallery()
+        throw new Error(response.status);
     }
 }
 
@@ -30,20 +35,37 @@ async function fetchImages(name) {
 const searchPhotoByName = (evt) => {
     evt.preventDefault();
     const inputName = refs.searchInput.value;
+ 
         
-    fetchImages(inputName).then(showImageGallery).catch(Notify.failure('Sorry, there are no images matching your search query. Please try again.'));
-}
+    fetchImages(inputName)
+        .then(showImageGallery)
+        .catch(() => {
+            Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        });
+    page += 1;
 
+
+}
+  
 
 refs.form.addEventListener('submit', searchPhotoByName);
 
 function showImageGallery(images) {
+        const totalHits = images.data.totalHits
+
+    if (images.data.hits.length === 0) {
+           Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+           clearImageGallery();
+           return;
+            }
     if (refs.searchInput.value === '') {
-        clearImageGallery()
-        return
+        clearImageGallery(images.data.hits)
+        return;
     } else {
-        renderMarkupImage(images)
+        Notify.success(`Hooray! We found ${totalHits} images.`)
+        renderMarkupImage(images.data.hits)
     }
+    
 }
 function renderMarkupImage(images) {
     const markup = images
@@ -78,7 +100,15 @@ function renderMarkupImage(images) {
             
         )
         .join('');
-    return (refs.imageGallery.innerHTML) = markup;
+
+    refs.imageGallery.innerHTML = markup;
+
+    const lightbox =  new SimpleLightbox('.gallery a', {
+        captionDelay: 250,
+        captionsData: 'alt',
+        captionPosition: "top",
+    });
+    lightbox.refresh();
 }
 
 function clearImageGallery() {
